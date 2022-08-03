f4d7ee826702e7b452c37964562878290bf397cf
Saltspeddy <walkercoco30@gmail.com>
2022-08-03 23:05:26 +0300

    adapted screen

--- script/main.js
+++ script/main.js
@@ -1,17 +1,12 @@
 // document.querySelector("#menu").style.width= "0px";
 document.querySelector("#menu").style.transform = "translatex(-150px)";
 let a = true;
-
 function toggleMenu(){
-    
     if(a == false){
         document.querySelector("#menu").style.transform = "translatex(-150px)";
-
     }
     if(a == true){
         document.querySelector("#menu").style.transform = "translatex(0px)";
-
     }
-
     a = !a;
 }
